e1d7353f3d4e2bbeedd2405c24902aa293f67051
Kxyudi <92844016+KXyudi@users.noreply.github.com>
2021-10-21 11:29:52 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281220670449', 'KurrTodあ', m)
-  m.reply('wa.me/6281220670449')
+  // this.sendContact(m.chat, '628873421382', 'gsattt', m)
+  this.sendContact(m.chat, '62881037558649', 'Kxyudi', m)
+  m.reply('wa.me/62881037558649')
 }
 handler.help = ['owner', 'creator', 'pemilikbot']
 handler.tags = ['info']
